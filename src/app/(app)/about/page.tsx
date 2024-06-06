2e1ed832b5399c19268fcffd68f1721c9fa21554
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2024-06-06 05:38:15 +0200

    feat: add nextui

--- src/app/(app)/about/page.tsx
+++ src/app/(app)/about/page.tsx
@@ -1,5 +1,17 @@
+import {
+	Button,
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	Link,
+} from "@nextui-org/react";
+
 export default function Page() {
 	return (
-		<h1 className="text-cyan-500 bg-violet-400 p-4 rounded-xl">About page</h1>
+		<main>
+			<h1 className="text-cyan-500 bg-violet-400 p-4 rounded-xl">About page</h1>
+			<Button>NextUI working fine</Button>
+		</main>
 	);
 }
